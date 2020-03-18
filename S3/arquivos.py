f608ae9c15a9036c3eed150b89c36d190b5cbdd7
= <=>
2020-03-18 16:45:03 -0300

    aula sobre arquivos

---
+++ S3/arquivos.py
@@ -0,0 +1,13 @@
+#Aula sobre arquivos
+meu_arquivo = open('texto.txt')
+print(meu_arquivo.read())
+#resentaod o ponteiro do arquivo
+meu_arquivo.seek(0)
+print("Voltando ao inicio do arquivo")
+print(meu_arquivo.readline())
+meu_arquivo.seek(0)
+
+#iterando dentro de um arquivo de texto
+for line in meu_arquivo:
+    print(line)
+

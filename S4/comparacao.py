e703ed9465991141bb7fa769952d1f3a467904cc
= <=>
2020-03-19 19:05:27 -0300

    aula sobre comparações

---
+++ S4/comparacao.py
@@ -0,0 +1,25 @@
+#aula sobre operadores de comparação
+#aula bem padrão, sem novos conhecimentos aqui
+print(1==1)
+print(1!=1)
+print('a' == 23)
+print(1>2)
+print(1>=1)
+#enfim
+#> , <, >=, <=, ==, !=
+
+
+#multiplas comparações booleanas: o && em C em python é and
+a = 1
+b = 2
+c = 3
+print("Comparação multiplas booleanas: a != b and b<c")
+print(a != b and b<c)
+
+#Operador or, semelhante ao || em C
+
+print("Resultado: a==b or b==c ")
+print(a==b or b==c)
+
+
+

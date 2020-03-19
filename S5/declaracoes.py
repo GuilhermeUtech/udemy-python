69bd0ba80646fbfb5aa27207a9d1378166013466
= <=>
2020-03-19 20:28:03 -0300

    pelizza é um amigo

---
+++ S5/declaracoes.py
@@ -0,0 +1,21 @@
+#Seção sobre declarações em python
+#if, else e blablabla. Python utiliza identação
+
+a = 12
+if a < 10:
+    print("True")
+else:
+    print("False")
+
+#Aula sobre if, elif e else
+banana = 10
+if banana < 10:
+    print("Menor que 10")
+elif banana > 10:
+    print("Maior que 10")
+else:
+    print("Sei lá manin")
+
+#
+
+

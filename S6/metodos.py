4fefa2a7c469054e4cc91be56a3a27a5601d6c69
= <=>
2020-03-20 15:49:42 -0300

    aula sobre métodos

---
+++ S6/metodos.py
@@ -0,0 +1,13 @@
+#Aula sobre métodos
+#funções que vivem dentro de objetos
+
+l = [1, 2, 3, 4, 5]
+l.append(6)
+print("append", l)
+l.pop()
+print("pop", l)
+l.reverse()
+print("reverse", l)
+
+#geral tem métodos e é isso ae!
+

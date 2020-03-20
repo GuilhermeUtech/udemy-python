8f00d057d734d27322e1e963d48e4cb054c2726c
= <=>
2020-03-20 11:56:51 -0300

    aula sobre range

---
+++ S5/range.py
@@ -0,0 +1,16 @@
+#Aula sobre o método range, range[aonde vai começar, até onde a gente vai - 1, opcional -> passo]
+#range = range(0,10) #é um gerador
+
+#print(list(range))
+#automaticamente cria a lista, hmmmm
+
+range2 = range(0,100,2)
+print(list(range2))
+
+
+#muito comum pra combinar com for
+
+for i in range(0,100):
+    print("Corona vírus")
+
+

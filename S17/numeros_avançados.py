d03427c3262fb4aa4a5517d3c90b703cc481f3f6
= <=>
2020-03-30 11:37:41 -0300

    aula sobre números e métodos sobre números avançados

---
+++ S17/numeros_avançados.py
@@ -0,0 +1,12 @@
+#Aula sobre números avançados
+
+print(hex(123)) #hexadecimal
+print(bin(123)) #binário
+print(2**2) #ao quadrado
+print(pow(5,2)) #ao quadrado
+print(round(3.12)) #arredondar
+print(round(3.99)) #arredondar
+
+#Existem uma série de métodos, quando precisar bastar olhar.
+
+

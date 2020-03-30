643aa6c381ccecdba8b457f94be93b3e6d679208
= <=>
2020-03-30 11:45:40 -0300

    aula sobre métodos avançados em strings

---
+++ S17/strings_avançadas.py
@@ -0,0 +1,23 @@
+s = "hello world"
+
+print(s.capitalize()) #primeira letra fica em maiúscula
+print(s.upper()) #tudo em maiúiscula
+print(s.lower()) #tudo em minússcula
+print(s.count("o")) #conta quantas letras passadas como argumento tem na minha string
+print(s.find("o")) #procura a posição que se encontra o "o"
+print(s.center(20,'z'))
+s = 'hello'
+print(s.isalnum())
+print(s.isalpha())
+print(s.islower())
+print(s.isupper())
+print(s.endswith("o"))
+print(s.split("e"))
+print(s.partition("e"))
+
+
+
+
+
+
+
